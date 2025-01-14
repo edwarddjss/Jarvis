2e6f0a267062a39ede6a3c11fb34d1c860b8cf42
nazk <edwardlongboat@gmail.com>
2025-01-14 04:50:01 -0500

    debugging

--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -1,6 +1,10 @@
 import { Client } from 'discord.js';
 import { readdirSync } from 'fs';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export async function loadEvents(client: Client) {
   const eventsPath = join(__dirname, '../events');
